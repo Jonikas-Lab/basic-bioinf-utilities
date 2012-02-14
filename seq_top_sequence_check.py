28a961bf7369ef10f3dc95229410a62e5021807c
Weronika Patena <patena@gmail.com>
2012-02-14 13:12:55 -0800

    New script to check the most common seqs/prefixes in file, with percentages.
    seq_top_sequence_check.py script - given any number of fasta or fastq
    files, print the N most common sequences in it (either full sequences,
    or Xbp prefixes) along with their counts as raw numbers and as
    percentages of the total count.  Optionally only print them if the
    percentage of the total count exceeds a minimum (not implemented).
    
    The output looks like this:
    
    mutant_pool_screens/1201_Ru_HiSeq_data >> seq_top_sequence_check.py cutadapt_RZ-1_pf_trimmed/*
     * cutadapt_RZ-1_pf_trimmed/06bp.fa (5543 seqs, 49 unique):
    88% CCTACT (4876), 2.3% GTCCCA (125), 2% ATTGCA (110)
     * cutadapt_RZ-1_pf_trimmed/10bp.fa (8292 seqs, 106 unique):
    80% GCACGAACCA (6593), 5.2% TATACGCAAC (430), 3% TAGCCGAGAC (251)
     * cutadapt_RZ-1_pf_trimmed/15bp.fa (30694 seqs, 312 unique):
    84% ACGGCTTTGTGTTGT (25915), 1.6% ACGGCTTTGTGTTGG (493), 0.55% CTGCCCGCTGCGGCA (168)
    
    mutant_pool_screens/1201_Ru_HiSeq_data >> seq_top_sequence_check.py -l 4 -n 5 cutadapt_RZ-1_pf_trimmed/*
     * cutadapt_RZ-1_pf_trimmed/06bp.fa (5543 seqs, 42 unique first 4bp):
    88% CCTA (4878), 2.3% GTCC (125), 2% ATTG (110), 1.9% CGAT (104), 1.1% TCCA (60)
     * cutadapt_RZ-1_pf_trimmed/10bp.fa (8292 seqs, 71 unique first 4bp):
    80% GCAC (6627), 5.2% TATA (431), 3.1% TAGC (254), 1.5% TCAC (123), 1.2% GGTT (99)
     * cutadapt_RZ-1_pf_trimmed/15bp.fa (30694 seqs, 139 unique first 4bp):
    86% ACGG (26548), 0.71% CTGC (218), 0.53% CCCA (163), 0.51% GCCT (156), 0.5% GCCG (152)
    
    Note the beauty of using str(float("%.2g"%x)) for formatting floats to 2
    significant digits instead of to a specific number of digits
    before/after the period!
    
    Tested, works.  TODO still needs the -m option implemented, and possibly
    other things like tabulated output and doing multiple sequence lengths
    at once.

---
+++ seq_top_sequence_check.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python
+"""
+Given any number of fasta or fastq files, print the N most common sequences in it (either full sequences, or Xbp prefixes) along with their counts as raw numbers and as percentages of the total count.  Optionally only print them if the percentage of the total count exceeds a minimum.
+NOTE: fastq file qualities will always be written using Sanger (+33) encoding! (HTSeq only implements that)
+ --Weronika Patena, Feb 2012
+"""
+
+# basic libraries
+import sys, os
+from collections import defaultdict
+# other libraries
+from HTSeq import FastaReader, FastqReader
+# my modules
+from seq_count_and_lengths import _format_lengths
+from deepseq_utilities import get_seq_count_from_collapsed_header
+
+
+def seq_top_sequence_check(infiles, seq_length=None, n_to_print=3, min_percent_to_print=None, input_collapsed_to_unique=False):
+    """ See module docstring and optparse option help messages, to avoid repeating the info. """
+
+    seqlen_info = ' first %sbp'%seq_length if seq_length else ''
+
+    for infile in infiles:
+        # file format recognition (I could do it by trying to use FastaReader/FastqReader on it, but it's annoying)
+        fasta_extensions = ['fa','fasta']
+        fastq_extensions = ['fq','fastq']
+        extension = os.path.splitext(infile)[1].lower()[1:]
+        if extension in fasta_extensions:   
+            infile_reader = FastaReader(infile)
+        elif extension in fastq_extensions: 
+            infile_reader = FastqReader(infile,qual_scale="solexa")
+        else:       
+            sys.exit("Error: input file %s (extension %s) needs to have a %s extension to be recognized!"%(infile, 
+                                extension, '/'.join(fasta_extensions+fastq_extensions)))
+
+        # a counter with a default value of 0
+        seq_counter = defaultdict(lambda: 0)
+
+        for sequence in infile_reader: 
+            N_seqs = get_seq_count_from_collapsed_header(sequence.name) if input_collapsed_to_unique else 1
+            subsequence = sequence.seq[0:seq_length]
+            seq_counter[subsequence] += N_seqs
+
+        seq_list_by_count = sorted(seq_counter.items(), key=lambda (s,c): c, reverse=True)
+
+        total_seqs = sum(seq_counter.values())
+
+        # if not using the min_percent_to_print option, just print the top N sequences from each file
+        if min_percent_to_print is None:
+            seq_data_list = []
+            for i in range(min(n_to_print,len(seq_list_by_count))):
+                seq,count = seq_list_by_count[i]
+                percent = count*100.0/total_seqs
+                # "%.2g" is significant-digit-based formatting of floats!!  So 92.12345 is 92%, but 0.00045 is 0.00045%.
+                percent_2_sig_digits = str(float("%.2g"%percent))
+                if percent_2_sig_digits.endswith(".0"):     percent_2_sig_digits = percent_2_sig_digits[:-2]
+                seq_data_list.append("%s%% %s (%d)"%(percent_2_sig_digits, seq, count))
+            print " * %s (%s seqs, %s unique%s):"%(infile, total_seqs, len(seq_list_by_count), seqlen_info) 
+            print ', '.join(seq_data_list)
+
+        # if using the min_percent_to_print option, just print the top N sequences from each file
+        else:
+            print "min_percent_to_print NOT IMPLEMENTED!"
+            # TODO implement!!
+
+
+    # MAYBE-TODO could also make it output stuff in a table for all the files?  Especially short prefixes...
+
+# MAYBE-TODO should this have unit-tests?
+
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+    parser = OptionParser(__doc__)
+    parser.add_option('-l','--seq_length', type='int', default=None, metavar='X',
+                      help="Print most common Xbp prefixes (None - full sequences) (default %default).")
+    parser.add_option('-n','--n_to_print', type='int', default=3, metavar='N',
+                      help="Print the N most common sequences (default %default).")
+    parser.add_option('-m','--min_percent_to_print', type='int', default=None, metavar='M',
+                      help="Only print the sequences that constitute over M% of total reads,"
+                          +"INSTEAD OF using -n (default %default).")
+    parser.add_option('-c','--input_collapsed_to_unique', action='store_true', default=False, 
+                      help="Use this to get correct total counts if the infile was collapsed to unique sequences using fastx_collapser, with original sequence counts encoded in the headers (a '>2-572' header means there were 572 identical sequences); default %default).")
+    (options, infiles) = parser.parse_args()
+
+    seq_top_sequence_check(infiles, options.seq_length, options.n_to_print, options.min_percent_to_print, options.input_collapsed_to_unique)
+
+    # MAYBE-TODO add an option to do multiple lengths per run, so that they're together for each file, instead of separate?  Like this:
+    #  * cutadapt_RZ-2_prev_trimmed/10bp.fa (7148 seqs, 77 unique, 21 unique first 4bp, 69 unique first 8bp):
+    #   88% ACTACCTACT (6262), 2.2% ACTAGTCCCA (159), 0.95% ACTACGATCT (68)
+    #   98% ACTA (6981), 0.7% GGGT (50), 0.34% ACCT (24), 0.24% ACTG (17), 0.22% AACA (16)
+    #   88% ACTACCTA (6263), 2.6% ACTAGTCC (186), 0.97% ACTACGAT (69), 0.8% ACTAGTAT (57), 0.7% GGGTCCAA (50)
+
