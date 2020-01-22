df468608e0d022e3f86e917638c0d9be461c63ad
Weronika Patena <patena@gmail.com>
2020-01-22 16:35:40 -0500

    Added colored text option to legend.

--- plotting_utilities.py
+++ plotting_utilities.py
@@ -79,9 +79,14 @@ def stacked_bar_plot(list_of_sample_category_lists, sample_names=[], bar_width=0
 
 # MAYBE-TODO could make the "if ax is None:  ax = mplt.gca(); else:  mplt.sca(ax)" line into a decorator, since it shows up everywhere
 
-def legend(*args, **kwargs):
-    """ make the legend with medium instead of large font size. """
-    mplt.legend(*args, prop=FontProperties(size='medium'), **kwargs)
+def legend(colortext=False, *args, **kwargs):
+    """ make the legend with optionally colored text, and medium instead of large font size. 
+    """
+    leg = mplt.legend(*args, prop=FontProperties(size='medium'), **kwargs)
+    if colortext:
+        for line,text in zip(leg.get_lines(), leg.get_texts()):
+            text.set_color(line.get_color())
+
 
 def remove_legend(ax=None):
     """ Remove legend for ax or the current axes (detected with gca()). """
